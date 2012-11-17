baa86a2fb7496de63d936e1fab98e776551d1996
Eduardo Arino de la Rubia <earino@gmail.com>
2012-11-17 14:37:17 -0800

    Initial commit

---
+++ myapp.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use Data::Dumper;
+use Mojolicious::Lite;
+
+# Documentation browser under "/perldoc"
+plugin 'PODRenderer';
+
+get '/' => sub {
+  my $self = shift;
+  $self->render(template => 'index', layout => 'base');
+};
+
+get '/nearby' => sub {
+  my $self = shift;
+  $self->render(template => 'nearby', layout => 'base');
+};
+
+post '/nearby' => sub {
+  my $self = shift;
+
+  my $name = $self->param('name');
+  my $motivation = $self->param('motivation');
+  my $new_dogs = $self->param('new-dogs');
+  my $new_people = $self->param('new-people');
+  my $walks = $self->param('walks');
+  my $when_called = $self->param('when-called');
+
+  $self->app->log->debug("name: $name");
+  $self->app->log->debug("motivation: $motivation");
+  $self->app->log->debug("new dogs: $new_dogs");
+  $self->app->log->debug("new people: $new_people");
+  $self->app->log->debug("walks: $walks");
+  $self->app->log->debug("when called: $when_called"); 
+
+  $self->render(template => 'nearby', layout => 'base');
+};
+
+app->start;
